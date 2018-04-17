d6c1a7f9cf64a09a1ff2ad5342e9fdd1c8d20a03
diegoballest <diego.alfredo96@gmail.com>
2018-04-17 16:54:07 -0500

    Parser

---
+++ ann.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+def parse_input():
+    d = int(input())
+    m = int(input())
+    n = int(input())
+
+    training_set = []
+    test_set = []
+
+    for i in range(m):
+        training_set.append(input().replace(' ','').split(','))
+    for i in range(n):
+        test_set.append(input().replace(' ','').split(','))
+
+    print(training_set)
+    print(test_set)
+
+parse_input();
